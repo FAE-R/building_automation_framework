15370f2e10b5df25ce7f47fb430af827b2aee695
payam fatehi <payam.fatehi@eonerc.rwth-aachen.de>
2024-06-26 13:31:47 +0200

    update tasks

--- ba_framework/agents/tasks.py
+++ ba_framework/agents/tasks.py
@@ -37,5 +37,34 @@ from django.db.models.signals import post_save, pre_save
 # Importing TaskResult model from django_celery_results to store the results of Celery tasks.
 from django_celery_results.models import TaskResult
 
+
+from .functions.occupancy_detection_task import Occupancy_Detection
+
 # Creating a logger for tasks in this module, which allows logging messages within tasks.
 logger = get_task_logger(__name__)
+
+
+@shared_task(bind=True, name="update_occupancy")
+def update_occupancy(self, room_name, *args, **kwargs):
+
+    room = Room.objects.get(name=room_name)
+
+    if room is not None:
+        current_state = 0
+
+        last_task_result = TaskResult.objects.filter(periodic_task_name=TaskResult.objects.get(task_id=self.request.id).periodic_task_name).last()
+
+        if last_task_result is not None:
+            if last_task_result.status == "SUCCESS":
+                current_state = int(last_task_result.result)
+            else:
+                current_state = 0
+        else:
+            current_state = 0 
+
+        result = Occupancy_Detection(room=room, current_state=current_state)
+        if result is not None:
+            return result
+
+    else:
+        return "Room object is None"
