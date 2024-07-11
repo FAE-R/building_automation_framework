42eb3e284dac7b30061440bb145a69fd46223a2c
sinakhodadad92@gmail.com <sinakhodadad92@gmail.com>
2024-07-11 09:36:12 +0200

    cleaning code and adding fixed docker compose file in dev

--- ba_framework/agents/tasks.py
+++ ba_framework/agents/tasks.py
@@ -2,22 +2,16 @@ from .models import Room
 from django.dispatch import receiver
 from celery import shared_task
 from ba_framework.celery import app
-# Importing get_task_logger from Celery to create loggers for tasks.
 from celery.utils.log import get_task_logger
-# Importing crontab schedule constructor from Celery for defining periodic tasks with cron-like schedules.
 from celery.schedules import crontab
-# Importing models from django_celery_beat for managing periodic tasks.
 from django_celery_beat.models import PeriodicTask, PeriodicTasks, CrontabSchedule, IntervalSchedule
 from django.apps import apps
-# Importing shared_task again from Celery (this is a duplicate import, and can be removed).
 from celery import shared_task
-# Importing TaskResult model from django_celery_results to store the results of Celery tasks.
 from django_celery_results.models import TaskResult
 
 
 from .functions.occupancy_detection_task import Occupancy_Detection
 
-# Creating a logger for tasks in this module, which allows logging messages within tasks.
 logger = get_task_logger(__name__)
 
 
