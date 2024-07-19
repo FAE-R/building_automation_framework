42729c58f83bdcea4f945b84b4ba2531861c2edb
sinakhodadad92@gmail.com <sinakhodadad92@gmail.com>
2024-07-19 15:26:24 +0200

    try to fetch data

--- ba_framework/ba_framework/celery.py
+++ ba_framework/ba_framework/celery.py
@@ -12,9 +12,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
-# Schedule the task to run every 10 minutes
+# Schedule the task to run every 1 minutes
 app.conf.beat_schedule = {
-    'fetch-data-every-10-minutes': {
+    'fetch-data-every-1-minutes': {
         'task': 'agents.tasks.fetch_data_task',
         'schedule': crontab(minute='*/1'),  # Runs every 1 minutes
         'args': (1,),  # Replace with appropriate room_id
